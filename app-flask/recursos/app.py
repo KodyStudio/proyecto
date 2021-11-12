3403a860d4f73b32cca4f14633d9a5181261c800
KodyStudio <justinsilvac360@gmail.com>
2021-11-12 02:06:13 -0600

    update no se cuanto

--- app-flask/recursos/app.py
+++ app-flask/recursos/app.py
@@ -1,19 +1,31 @@
 from flask import Flask, render_template
-app = Flask(__name__)  
- 
-@app.route('/')  
-def home():  
-    return render_template('index.html'); 
-
-@app.route('/home/add_product')  
-def add_product():  
-    return render_template('page-add-product.html');   
-
-@app.route('/home/error')  
-def error():  
-    return render_template('404.html');  
-  
-if __name__ =="__main__":  
-    app.run(debug = True)  
+app = Flask(__name__)
 
 
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/home/add_product')
+def add_product():
+    return render_template('page-add-product.html')
+
+
+@app.route('/home/error')
+def error():
+    return render_template('404.html')
+
+
+@app.route('/home/base')
+def base():
+    return render_template('base.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
