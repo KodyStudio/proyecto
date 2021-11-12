8cb6fb2547889de8577b3b631699d337cf5bfcd7
KodyStudio <justinsilvac360@gmail.com>
2021-11-12 02:06:13 -0600

    update no se cuanto

--- app-flask/recursos/controllers/controller.py
+++ app-flask/recursos/controllers/controller.py
@@ -2,12 +2,13 @@ from flask import Flask, render_template, request
 from recursos.db import mysql
 from flask.views import MethodView
 
+
 def post(self):
     code = request.form['code']
     name = request.form['name']
     stock = request.form['stock']
     value = request.form['value']
-    category = request.form['category']   
-    
-    print(code, name, stock, value, category)     
+    category = request.form['category']
+
+    print(code, name, stock, value, category)
     return "method post is work"
